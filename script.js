5ae3445b354ebda1bf852b1fd963e7292ad1ff4d
Apsana <apsana20580912@gmail.com>
2024-09-23 07:01:29 +0545

    localStorageAdded

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const inputFields=document.querySelectorAll('.goal-list')
 const errorLabel=document.querySelector('.errorlabel')
 const progressBar=document.querySelector('.progressBar')
 const progressValue=document.querySelector('.progress-value')
+
+const allGoals=JSON.parse(localStorage.getItem('allGoals')) ||{}
+
+
+
+
+
+
 checkboxList.forEach((checkbox)=>{
     checkbox.addEventListener('click',(e)=>{
         const allFieldsFilled=[...inputFields].every(function(input){
@@ -10,7 +18,11 @@ checkboxList.forEach((checkbox)=>{
         })
         if (allFieldsFilled){
         checkbox.parentElement.classList.toggle("completed");
-        progressValue.style.width="45%"
+        // progressValue.style.width="45%"
+        const inputId=checkbox.nextElementSibling.id
+        allGoals[inputId].completed=!allGoals[inputId].completed
+        localStorage.setItem('allGoals' ,JSON.stringify(allGoals))
+
         // errorLabel.style.display = 'none'; // Hide the error label
         // progressBar.classList.remove('show-error');
 
@@ -24,7 +36,11 @@ checkboxList.forEach((checkbox)=>{
     });
 });
 inputFields.forEach((input)=>{
-   
+//    console.log(  allGoals[input.id])
+   input.value= allGoals[input.id].name
+   if(allGoals[input.id].completed){
+    input.parentElement.classList.add('completed')
+   }
         if (input.value.trim() !== '') {
             input.style.color = 'black'; 
         } else {
@@ -36,11 +52,16 @@ inputFields.forEach((input)=>{
             input.style.color='black';
         }
         progressBar.classList.remove('show-error');
-    });
-    input.addEventListener('light-black', () => {
-        if (input.value.trim() === '') {
-            input.style.color = '#D9D9D9'; 
-        }})
+    })
+
+    // input.addEventListener('blur', () => {
+    //     if (input.value.trim() === '') {
+    //         input.style.color = '#D9D9D9'; 
+    //     }})
     
+    input .addEventListener('input',(e)=>{
+        allGoals[input.id]={name:input.value,completed:false,}
+     localStorage.setItem('allGoals' ,JSON.stringify(allGoals))
+    })
         
 })
